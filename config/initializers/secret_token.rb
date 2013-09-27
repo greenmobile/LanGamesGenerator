d50956ab378f4a234a75f328daca67f5c285d914
Tillmann <heigel@gmino.de>
2013-09-27 21:51:31 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LanGamesGenerator::Application.config.secret_key_base = '50ee08850c8d53feec214cc1b9958f98cea7e80ddb327586b740c37a90c030a2e898f61347223dd9cbb5891cd58a02585b503426442b7a72cb245015edb40ca5'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+LanGamesGenerator::Application.config.session_store :cookie_store, key: '_LanGamesGenerator_session'
